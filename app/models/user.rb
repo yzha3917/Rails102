cfe5b74777e5cc702893dac7c08a009e9f9d4dfa
yzha3917 <boston.zhang@gmail.com>
2016-10-11 15:16:37 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
     participated_groups.include?(group)
   end
 
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
+  
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
   has_many :posts
